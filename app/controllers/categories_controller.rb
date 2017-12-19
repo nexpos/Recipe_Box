90337645143a0ab9d3b23843f6224d14246271f3
Nick Exposito <nexpos@gmail.com>
2017-12-19 10:38:10 -0700

    updated categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
+  authorize_resource
+
+
   # GET /categories
   # GET /categories.json
   def index
